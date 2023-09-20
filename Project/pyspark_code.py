bacd9ba19704c4a0d4473ecd6f63a574df1f1f14
Mayur Mahanta <mayur.mahantaprof@gmail.com>
2023-09-20 19:03:26 -0400

    Update pyspark_code.py

--- Project/pyspark_code.py
+++ Project/pyspark_code.py
@@ -4,8 +4,6 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-
-############################### Added by Carlos ###############################
 from awsglue.dynamicframe import DynamicFrame
 
 
@@ -51,7 +49,7 @@ dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ct
 
 datasink1 = dropnullfields3.toDF().coalesce(1)
 df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast2-mayurdev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
 
 ############################### Added by Darshil ###############################
 job.commit()
